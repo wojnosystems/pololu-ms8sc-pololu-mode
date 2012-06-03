b326f1e3daef37a190258a96734aada65818e40d
Christopher Richard Wojno <cwojno@farcode.com>
2012-06-03 10:05:07 -0700

    converted procedural default value setting to compile-time buffer default values

--- CWPololuSerialServo.cpp
+++ CWPololuSerialServo.cpp
@@ -10,12 +10,9 @@ int CwPololuSerialServo::setSpeed(
 				unsigned short int servo_number,
 				unsigned short int speed
 	) {
-	unsigned char cmd[5];
+	unsigned char cmd[5] = { 0x80, 0x01, 0x01, 0x00, 0x00 };
 	if( !CwPololuSerialServo::isValidServoId(servo_number) ) return -2;
 	if( speed > 127 ) return -3;
-	cmd[0] = 0x80; // start byte
-	cmd[1] = 0x01; // device id
-	cmd[2] = 0x01; // command
 	cmd[3] = lowByte(servo_number);
 	cmd[4] = lowByte(speed);
 	_serial->write( cmd, 5 );
@@ -51,12 +48,9 @@ int CwPololuSerialServo::setPositionAbsolute(
 				unsigned short int servo_number,
 				unsigned int position
 	) {
-	unsigned char cmd[6]; // need enough for 8-bit version
+	unsigned char cmd[6] = { 0x80, 0x01, 0x04, 0x00, 0x00, 0x00 }; // need enough for 8-bit version
 	if( !CwPololuSerialServo::isValidServoId(servo_number) ) return -2;
 	if( position > 5500 || position < 500 ) return -3;
-	cmd[0] = 0x80; // start byte
-	cmd[1] = 0x01; // device id
-	cmd[2] = 0x04; // command
 	cmd[3] = servo_number;
 	// upper data bits: shift off lower bits
 	cmd[4] = lowByte((position>>7));
@@ -74,14 +68,10 @@ int CwPololuSerialServo::configureServo(
 				bool forward_direction,
 				unsigned short int range
 	) {
-	unsigned char cmd[5];
+	unsigned char cmd[5] = { 0x80, 0x01, 0x00, 0x00, 0x00 };
 	if( !CwPololuSerialServo::isValidServoId(servo_number) ) return -2;
 	if( range > 15 ) return -3;
-	cmd[0] = 0x80; // start byte
-	cmd[1] = 0x01; // device id
-	cmd[2] = 0x00; // command
 	cmd[3] = servo_number;
-	cmd[4] = 0x00;
 	if( enabled ) { bitSet(cmd[4],6); }
 	if( !forward_direction ) { bitSet(cmd[4],5); }
 	cmd[4] = (cmd[4] | range);
@@ -90,14 +80,28 @@ int CwPololuSerialServo::configureServo(
 	return 0;
 }
 
+int CwPololuSerialServo::setNeutral(
+	unsigned short int servo_number,
+	unsigned int position ) {
+	unsigned char cmd[6] = { 0x80, 0x01, 0x05, 0x00, 0x00, 0x00 };
+	if( !CwPololuSerialServo::isValidServoId(servo_number) ) return -2;
+	if( position > 5500 || position < 500 ) return -3;
+	cmd[3] = servo_number;
+	// upper data bits: shift off lower bits
+	cmd[4] = lowByte((position>>7));
+	// lower data 7-bits
+	cmd[5] = lowByte(0x7f & position); // bitmask: 0111_1111 = 7f
+	_serial->write( cmd, 6 );
+	_serial->flush();
+	return 0;
+}
+
 int CwPololuSerialServo::setServoNumberBaseNumber(
 		unsigned short int base_number )
 {
-	unsigned char cmd[3];
+	unsigned char cmd[3] = { 0x80, 0x02, 0x00 };
 	// check validity of the base number.
 	if( base_number > 0x0f ) return -2;
-	cmd[0] = 0x80;
-	cmd[1] = 0x02;
 	cmd[2] = base_number;
 	_serial->write( cmd, 3 );
 	_serial->flush();
@@ -106,11 +110,8 @@ int CwPololuSerialServo::setServoNumberBaseNumber(
 
 void CwPololuSerialServo::blinkServoNumberBaseNumber() const
 {
-	unsigned char cmd[3];
+	unsigned char cmd[3] = { 0x80, 0x02, 0x0f };
 	// check validity of the base number.
-	cmd[0] = 0x80;
-	cmd[1] = 0x02;
-	cmd[2] = 0x0f;
 	_serial->write( cmd, 3 );
 	_serial->flush();
 }
